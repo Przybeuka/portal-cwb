c1932d5558bb6f028390a90695c0dda3cf0c7ce8
Przybeuka <jeander.kauanp@gmail.com>
2025-04-05 16:20:44 -0300

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -15,17 +16,26 @@ const nextConfig = {
       },
     ],
     formats: ['image/avif', 'image/webp'],
+    domains: ['localhost', 'portalcwb.com.br'],
   },
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000'],
     },
+    optimizeCss: true,
   },
   env: {
     STRAPI_API_URL: process.env.STRAPI_API_URL,
     STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
   },
 }
 
